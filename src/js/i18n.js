355650342f9c45767c88ff8e949f9fec245b6264
tommmasotacconi <tommaso.tacconi@gmail.com>
2026-06-19 17:41:06 +0200

    feat: Default to en locale

--- src/js/i18n.js
+++ src/js/i18n.js
@@ -3,7 +3,7 @@ import it from '../locales/it.json'; // italian text
 import en from '../locales/en.json'; // english text
 
 export default createI18n({
-  locale: 'it',
+  locale: 'en',
   fallbackLocale: 'en',
   messages: {
     it,
